dd5a85cbc1fe634ccf0ba6f75a50b5429bbc4d74
andrewakagawa <andrewakagawa@gmail.com>
2014-05-03 21:29:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ function watchTimer(){
 }
 
 function getDistance(lat1,lon1,lat2,lon2) {
-	var R = 6371000; // Radius of the earth in km
+	var R = 6371; // Radius of the earth in m
 	var dLat = deg2rad(lat2-lat1);  // deg2rad below
 	var dLon = deg2rad(lon2-lon1); 
 	var a = 
