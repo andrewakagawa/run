e075bbddb07bbfcff582f70c518a60ab76942a59
Andrew Akagawa <andrewakagawa@gmail.com>
2014-05-04 03:07:24 +0000

    doing crazy things with geo

--- app.js
+++ app.js
@@ -97,6 +97,7 @@ function watchTimer(){
 	total_dist += dist;
 
         //print to the app
+	document.getElementById('track').innerHTML = 'time: ' + elapsed + '<br>speed: ' + speed + '<br>distance: ' + total_dist;	
 	document.getElementById('geo').innerHTML = 'time: ' + elapsed + '<br>speed: ' + speed + '<br>distance: ' + total_dist;	
 
         //reset lat,lng,time for next watch
