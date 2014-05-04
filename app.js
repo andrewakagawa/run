4bf71ee46d5565542148674b5775a81d9760c3b0
Andrew Akagawa <andrewakagawa@gmail.com>
2014-05-04 01:58:32 +0000

    test

--- app.js
+++ app.js
@@ -80,22 +80,22 @@ function watchLocation(){
 
 function start() {	
 	timestamp = new Date();
-        init_lat = lat;
-        init_lng = lng;
-
+        last_lat = lat;
+        last_lng = lng;
 }
 
 function watchTimer(){
 	time = new Date();
 	var elapsed = time - timestamp;
-
-        var dist = getDistance(init_lat, init_lng, lat, lng);
-
+        var dist = getDistance(last_lat, last_lng, lat, lng);
 	document.getElementById('geo').innerHTML = 'time: ' + elapsed + '<br>test: '+ time +'<br>speed: ' + speed + '<br>distance: ' + dist;	
+        last_lat = lat;
+        last_lng = lng;
+
 }
 
 function getDistance(lat1,lon1,lat2,lon2) {
-	var R = 6371; // Radius of the earth in km
+	var R = 6.371; // Radius of the earth in km
 	var dLat = deg2rad(lat2-lat1);  // deg2rad below
 	var dLon = deg2rad(lon2-lon1); 
 	var a = 
@@ -104,7 +104,7 @@ function getDistance(lat1,lon1,lat2,lon2) {
 	  Math.sin(dLon/2) * Math.sin(dLon/2)
 	  ; 
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-	var d = R * c; // Distance in km
+	var d = R * c; // Distance in meters
 	return d;
 }
 
